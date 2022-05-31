76dbff2753c4c21d111cdf801faa141476964b1d
sas5c27 <105262749+sas5c27@users.noreply.github.com>
2022-05-31 11:43:26 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,25 @@
+music.setVolume(255)
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    serial.writeLine("" + (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
+    basic.showNumber(sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ))
+    if (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ) > 5) {
+        music.playTone(988, music.beat(BeatFraction.Half))
+        basic.pause(200)
+        music.playTone(988, music.beat(BeatFraction.Half))
+    }
+})
